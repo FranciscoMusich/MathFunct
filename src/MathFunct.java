939f391a37d09747af09f28426b67101644affac
Francisco Musich <franmusich@gmail.com>
2017-05-15 17:35:04 -0300

    Upload desde eclipse

---
+++ src/MathFunct.java
@@ -0,0 +1,29 @@
+
+public class MathFunct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long startTime;
+		long endTime;
+		double Time; 
+		startTime = System.currentTimeMillis();
+		double base, height, area;
+		base = 10;
+		height = 5;
+		area = (base * height)/2;
+		System.out.println("El area es = " + area);
+		double randomNum; 
+		randomNum = Math.random();
+		System.out.println("Numero al Azar = "+ randomNum);
+		System.out.println("Este es el numero PI" + Math.PI);
+		endTime = System.currentTimeMillis();
+		Time = (endTime - startTime)/1000;
+		System.out.println("El tiempo de operacion fue = " + Time);
+		
+		
+				
+			
+
+	}
+
+}
---
+++ src/Strings.java
@@ -0,0 +1,27 @@
+
+public class Strings {
+	enum Seasons {Spring, Summer, Fall, Winter};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String consejo;
+		String consejo2;
+		boolean comp;
+		consejo = "Aprovecha el dia!";
+		System.out.println(consejo.length());
+		consejo2 = "Hola Hola";
+		comp = consejo.equals(consejo2);
+		if (comp == false);{
+			System.out.println("No son iguales");
+		Seasons temporada ;
+		temporada = Seasons.Summer;
+		System.out.println(temporada);;
+		
+		
+		
+		}
+	
+	
+	}
+
+}
